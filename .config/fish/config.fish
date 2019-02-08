ca39f6c4f97e58eb361c311266589cb0c18bf7d9
u1aryz <u1aryz.d@gmail.com>
2019-02-09 01:18:37 +0900

    fishのconfファイルを追加

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+set -g theme_date_format "+%H:%M:%S"
+
+# エイリアス
+balias gst 'git status'
+balias ga 'git add'
+balias gc 'git commit'
+balias gb 'git branch'
+balias gco 'git checkout'
+balias gd 'git diff'
+balias gf 'git fetch'
+balias gdc 'git diff --cached'
+balias gg 'git graph'
+balias tmux 'tmux -u'
+balias l 'ls -la'
+balias mkdir 'mkdir -p'
