9268f27a02b3618dfe0099ef02e714e179a51793
u1aryz <u1aryz.d@gmail.com>
2019-02-15 17:44:55 +0900

    fzf移動後にコマンドラインをクリアする処理を追加

--- .fish/config.fish
+++ .fish/config.fish
@@ -7,6 +7,8 @@ function __fzf_z -d "Change directory by z & fzf"
   eval "z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --query (commandline) | sed 's/^[0-9,.]* *//'" | read -l select
   if not test -z "$select"
     cd $select
+    # clear
+    commandline -t ""
   end
   commandline -f repaint
 end
