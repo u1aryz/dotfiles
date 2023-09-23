b592c65ea4176f4fca186e0e31ea3df019e7a93d
u1aryz <u1aryz.d@gmail.com>
2023-09-24 01:35:41 +0900

    ファイル検索にfdを使用する設定を追加

--- .fish/config.fish
+++ .fish/config.fish
@@ -1,8 +1,8 @@
 # plugin settings
 # fzfの古いキーバインドは使わない
 set -U FZF_LEGACY_KEYBINDINGS 0
-# ファイル検索にrgを使用する
-set -U FZF_FIND_FILE_COMMAND "rg --files --hidden --follow --glob '!.git/*' 2> /dev/null"
+# ファイル検索にfdを使用する
+set -U FZF_FIND_FILE_COMMAND "fd --type file --no-ignore --follow --hidden --exclude .git"
 set -U FZF_OPEN_COMMAND $FZF_FIND_FILE_COMMAND
 set -g theme_date_timezone Asia/Tokyo
 set -g theme_date_format "+%H:%M:%S"
@@ -108,3 +108,4 @@ alias ls 'exa'
 alias grep 'rg'
 alias cat 'bat -pP'
 alias less 'bat'
+alias find 'fd'
