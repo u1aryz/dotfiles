02d22e703c76e0f468b1359cbe508fce7abb6bfe
u1aryz <u1aryz.d@gmail.com>
2025-07-02 18:41:02 +0900

    画面をスリープするfish関数を追加

--- .fish/config.fish
+++ .fish/config.fish
@@ -74,6 +74,23 @@ function __fzf_delete_branch -d "Delete git branch by fzf"
     end
 end
 
+function sleep_display --description "Sleep the display after X seconds (default: 2) while keeping the system awake"
+    # Default delay is 2 seconds
+    set -l delay 2
+    if test (count $argv) -gt 0
+        set delay $argv[1]
+    end
+
+    # Start a single caffeinate keeper if none is running (prevent idle/system/disk sleep, allow display sleep)
+    if not pgrep -x caffeinate >/dev/null
+        caffeinate -ims &
+        disown
+    end
+
+    sleep $delay
+    pmset displaysleepnow
+end
+
 # keybind
 bind \ch __fzf_z
 bind \cg __ghq_repository_search
