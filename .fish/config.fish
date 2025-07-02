763ac41c1d1b626e811a84a4556e1bce65ea73b2
u1aryz <u1aryz.d@gmail.com>
2025-07-02 18:36:48 +0900

    config.fishをフォーマット

--- .fish/config.fish
+++ .fish/config.fish
@@ -35,48 +35,48 @@ set -U fish_pager_color_prefix white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
 set -U fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
 
 function fish_greeting
-  fish_logo blue cyan green '[' '@' | lolcat -p 3.0 -F 0.28 -S 1
+    fish_logo blue cyan green '[' '@' | lolcat -p 3.0 -F 0.28 -S 1
 end
 
 function __fzf_z -d "Change directory by z & fzf"
-  eval "z -l 2>&1 | fzf +s --query (commandline) | sed 's/^[0-9,.]* *//'" | read -l select
-  if not test -z "$select"
-    cd $select
-    # clear
-    commandline -t ""
-  end
-  commandline -f repaint
+    eval "z -l 2>&1 | fzf +s --query (commandline) | sed 's/^[0-9,.]* *//'" | read -l select
+    if not test -z "$select"
+        cd $select
+        # clear
+        commandline -t ""
+    end
+    commandline -f repaint
 end
 
 function __fzf_checkout -d "Checkout git branch by fzf"
-  set -l branches (git branch --all)
-  set -l result $status
-  if test $result -ne 0
-    return $result
-  end
+    set -l branches (git branch --all)
+    set -l result $status
+    if test $result -ne 0
+        return $result
+    end
 
-  eval "printf '%s\n' \$branches | grep -v HEAD | sed 's/.* //' | sed 's#remotes/[^/]*/##' | awk '!a[\$0]++' | fzf --query \"$argv\"" | read -l select
-  if not test -z "$select"
-    git checkout $select
-  end
+    eval "printf '%s\n' \$branches | grep -v HEAD | sed 's/.* //' | sed 's#remotes/[^/]*/##' | awk '!a[\$0]++' | fzf --query \"$argv\"" | read -l select
+    if not test -z "$select"
+        git checkout $select
+    end
 end
 
 function __fzf_delete_branch -d "Delete git branch by fzf"
-  set -l branches (git branch)
-  set -l result $status
-  if test $result -ne 0
-    return $result
-  end
+    set -l branches (git branch)
+    set -l result $status
+    if test $result -ne 0
+        return $result
+    end
 
-  eval "printf '%s\n' \$branches | sed 's/.* //' | fzf --query \"$argv\"" | read -l select
-  if not test -z "$select"
-    git branch -d $select
-  end
+    eval "printf '%s\n' \$branches | sed 's/.* //' | fzf --query \"$argv\"" | read -l select
+    if not test -z "$select"
+        git branch -d $select
+    end
 end
 
 # keybind
-bind \ch '__fzf_z'
-bind \cg '__ghq_repository_search'
+bind \ch __fzf_z
+bind \cg __ghq_repository_search
 
 # env
 set -x ANDROID_HOME ~/Library/Android/sdk
@@ -102,12 +102,12 @@ alias gg 'git graph'
 alias tmux 'tmux -u'
 alias l 'ls -la'
 alias mkdir 'mkdir -p'
-alias fco '__fzf_checkout'
-alias fbd '__fzf_delete_branch'
+alias fco __fzf_checkout
+alias fbd __fzf_delete_branch
 alias gghq 'GHQ_ROOT=~/go/src ghq'
-alias ls 'eza'
-alias grep 'rg'
+alias ls eza
+alias grep rg
 alias cat 'bat -pP'
-alias less 'bat'
-alias find 'fd'
+alias less bat
+alias find fd
 alias mr 'mise run'
