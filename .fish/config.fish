99df166d32f7917dbf07e8b46a5f9a7418de2606
u1aryz <u1aryz.d@gmail.com>
2025-10-12 13:57:57 +0900

    Bitwarden SSH agentを使用する設定を追加

--- .fish/config.fish
+++ .fish/config.fish
@@ -130,6 +130,7 @@ set -x ANDROID_HOME ~/Library/Android/sdk
 set -x EDITOR 'code --new-window'
 set -x FZF_DEFAULT_OPTS '--height 40% --reverse --inline-info'
 set -x RUNEWIDTH_EASTASIAN 0
+set -x SSH_AUTH_SOCK ~/.bitwarden-ssh-agent.sock
 fish_add_path $ANDROID_HOME/tools
 fish_add_path $ANDROID_HOME/platform-tools
 fish_add_path ~/go/bin
