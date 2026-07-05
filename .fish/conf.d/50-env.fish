c863fe1b9f5baaf1a54a807f58b264a7f2b1ce51
u1aryz <u1aryz.d@gmail.com>
2026-07-05 17:23:41 +0900

    fish の環境パスを更新

--- .fish/conf.d/50-env.fish
+++ .fish/conf.d/50-env.fish
@@ -1,11 +1,21 @@
-# env
-set -x ANDROID_HOME ~/Library/Android/sdk
-set -x EDITOR 'code --new-window'
-set -x FZF_DEFAULT_OPTS '--height 40% --reverse --inline-info'
-set -x RUNEWIDTH_EASTASIAN 0
-set -x SSH_AUTH_SOCK ~/.bitwarden-ssh-agent.sock
-fish_add_path $ANDROID_HOME/tools
+# Environment variables
+set -gx ANDROID_HOME /opt/homebrew/share/android-commandlinetools
+set -gx JAVA_HOME /opt/homebrew/opt/openjdk
+set -gx EDITOR 'code --new-window'
+set -gx FZF_DEFAULT_OPTS '--height 40% --reverse --inline-info'
+set -gx RUNEWIDTH_EASTASIAN 0
+set -gx SSH_AUTH_SOCK ~/.bitwarden-ssh-agent.sock
+
+# Java
+fish_add_path $JAVA_HOME/bin
+
+# Android SDK
+fish_add_path $ANDROID_HOME/cmdline-tools/latest/bin
+fish_add_path $ANDROID_HOME/emulator
 fish_add_path $ANDROID_HOME/platform-tools
-fish_add_path ~/go/bin
+fish_add_path $ANDROID_HOME/tools
+
+# User tools
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/share/mise/shims
+fish_add_path ~/go/bin
