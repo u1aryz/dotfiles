63d6b7c6743799f2495b46a46840736b2536dab1
u1aryz <u1aryz.d@gmail.com>
2026-08-24 23:17:37 +0900

    chore: piのabbr設定を削除

--- .fish/conf.d/90-abbreviations.fish
+++ .fish/conf.d/90-abbreviations.fish
@@ -27,7 +27,6 @@ abbr -a mkdir mkdir -p
 abbr -a mr mise run
 abbr -a ml mise list
 abbr -a mi mise install
-abbr -a pi pi --permission-mode auto
 
 # function aliases (keep as alias since they call functions)
 alias fco __fzf_checkout
