75c8c2fc5a429588e9fcbeee11438a1952c02866
u1aryz <u1aryz.d@gmail.com>
2026-08-29 23:41:17 +0900

    feat: pi自動許可モード用abbrの追加

--- .fish/conf.d/90-abbreviations.fish
+++ .fish/conf.d/90-abbreviations.fish
@@ -27,6 +27,7 @@ abbr -a mkdir mkdir -p
 abbr -a mr mise run
 abbr -a ml mise list
 abbr -a mi mise install
+abbr -a pia pi --permission-mode auto
 
 # function aliases (keep as alias since they call functions)
 alias fco __fzf_checkout
